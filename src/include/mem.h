a34f4a27328df1a62cb376ecac7e23d6732d2269
Vera Mitchell <vgm@apple.com>
2026-03-18 11:17:00 -0600

    add standalone allocator wrapper functions
    in order for the typed memory operations use of typed allocators to
    work, calls to allocator functions need to be decorated with the
    _MALLOC_TYPED attribute. since cmark accesses its allocator wrappers
    through function pointers and the cmark_mem struct, this commit adds the
    groundwork necessary to allow the typed allocator wrappers to be called
    correctly.

--- src/include/mem.h
+++ src/include/mem.h
@@ -14,6 +14,25 @@ extern "C" {
 # define CMARK_MALLOC_TYPED(F,N)
 #endif
 
+CMARK_GFM_NO_EXPORT
+void *cmark_mem_calloc_typed(cmark_mem *mem, size_t count, size_t size, cmark_malloc_type_id type_id);
+
+#define cmark_mem_calloc_typed_backdeploy cmark_mem_calloc_typed
+
+CMARK_GFM_NO_EXPORT
+void *cmark_mem_realloc_typed(cmark_mem *mem, void *ptr, size_t size, cmark_malloc_type_id type_id);
+
+#define cmark_mem_realloc_typed_backdeploy cmark_mem_realloc_typed
+
+CMARK_GFM_NO_EXPORT
+void *cmark_mem_calloc(cmark_mem *mem, size_t count, size_t size) CMARK_MALLOC_TYPED(cmark_mem_calloc_typed, 3);
+
+CMARK_GFM_NO_EXPORT
+void *cmark_mem_realloc(cmark_mem *mem, void *ptr, size_t size) CMARK_MALLOC_TYPED(cmark_mem_realloc_typed, 3);
+
+CMARK_GFM_NO_EXPORT
+void cmark_mem_free(cmark_mem *mem, void *ptr);
+
 #ifdef __cplusplus
 }
 #endif
---
+++ src/mem.c
@@ -0,0 +1,32 @@
+#include "cmark-gfm.h"
+#include "mem.h"
+
+void *cmark_mem_calloc(cmark_mem *mem, size_t count, size_t size) {
+  return mem->calloc(count, size);
+}
+
+void *cmark_mem_realloc(cmark_mem *mem, void *ptr, size_t size) {
+  return mem->realloc(ptr, size);
+}
+
+void cmark_mem_free(cmark_mem *mem, void *ptr) {
+  mem->free(ptr);
+}
+
+#if defined(_MALLOC_TYPE_ENABLED) && _MALLOC_TYPE_ENABLED
+
+void *cmark_mem_calloc_typed(cmark_mem *mem, size_t count, size_t size, malloc_type_id_t type_id) {
+  if (mem->calloc_typed)
+    return mem->calloc_typed(count, size, type_id);
+
+  return mem->calloc(count, size);
+}
+
+void *cmark_mem_realloc_typed(cmark_mem *mem, void *ptr, size_t size, malloc_type_id_t type_id) {
+  if (mem->realloc_typed)
+    return mem->realloc_typed(ptr, size, type_id);
+
+  return mem->realloc(ptr, size);
+}
+
+#endif
