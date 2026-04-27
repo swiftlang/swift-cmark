3c475972e126b9a5bb703bf570137d168cdbc269
Vera Mitchell <vgm@apple.com>
2026-04-27 09:43:20 -0600

    add fallback 'cmark_mem_typed' functions

--- src/mem.c
+++ src/mem.c
@@ -15,18 +15,28 @@ void cmark_mem_free(cmark_mem *mem, void *ptr) {
 
 #if defined(_MALLOC_TYPE_ENABLED) && _MALLOC_TYPE_ENABLED
 
-void *cmark_mem_calloc_typed(cmark_mem *mem, size_t count, size_t size, malloc_type_id_t type_id) {
+void *cmark_mem_calloc_typed(cmark_mem *mem, size_t count, size_t size, cmark_malloc_type_id type_id) {
   if (mem->calloc_typed)
     return mem->calloc_typed(count, size, type_id);
 
   return mem->calloc(count, size);
 }
 
-void *cmark_mem_realloc_typed(cmark_mem *mem, void *ptr, size_t size, malloc_type_id_t type_id) {
+void *cmark_mem_realloc_typed(cmark_mem *mem, void *ptr, size_t size, cmark_malloc_type_id type_id) {
   if (mem->realloc_typed)
     return mem->realloc_typed(ptr, size, type_id);
 
   return mem->realloc(ptr, size);
+}
+
+#else
+
+void *cmark_mem_calloc_typed(cmark_mem *mem, size_t count, size_t size, cmark_malloc_type_id type_id) {
+  return cmark_mem_calloc(mem, count, size);
+}
+
+void *cmark_mem_realloc_typed(cmark_mem *mem, void *ptr, size_t size, cmark_malloc_type_id type_id) {
+  return cmark_mem_realloc(mem, ptr, size);
 }
 
 #endif
