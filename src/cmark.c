6ec2ec4687034d7032f8be37e68fa7beba27efbe
Vera Mitchell <vgm@apple.com>
2026-04-27 10:21:03 -0600

    suppress the allocator wrappers warning for the allocator wrappers
    Now that the wrappers are added to the type-aware allocator machinery,
    we can silence the allocator wrapper warning (which still fires here) to
    prevent future false-positives.

--- src/cmark.c
+++ src/cmark.c
@@ -8,6 +8,12 @@
 #include "buffer.h"
 #include "mem.h"
 
+#define START_TYPE_ALLOCATOR_IMPL \
+  _Pragma("clang diagnostic push") \
+  _Pragma("clang diagnostic ignored \"-Wallocator-wrappers\"")
+#define END_TYPE_ALLOCATOR_IMPL \
+  _Pragma("clang diagnostic pop")
+
 cmark_node_type CMARK_NODE_LAST_BLOCK = CMARK_NODE_FOOTNOTE_DEFINITION;
 cmark_node_type CMARK_NODE_LAST_INLINE = CMARK_NODE_ATTRIBUTE;
 
@@ -22,6 +28,10 @@ const char *cmark_version_string(void) { return CMARK_GFM_VERSION_STRING; }
 // the macOS system headers
 __attribute__((weak_import)) void * __sized_by_or_null(count * size) malloc_type_calloc(size_t count, size_t size, malloc_type_id_t type_id) __result_use_check __alloc_size(1,2);
 __attribute__((weak_import)) void * __sized_by_or_null(size) malloc_type_realloc(void * __unsafe_indexable ptr, size_t size, malloc_type_id_t type_id) __result_use_check __alloc_size(2);
+
+// These are the typed allocator versions of the allocator wrappers.
+// The typed-operation behavior is managed in mem.c
+START_TYPE_ALLOCATOR_IMPL
 
 static void *xcalloc_typed(size_t nmem, size_t size, cmark_malloc_type_id type_id) {
   void *ptr;
@@ -64,6 +74,8 @@ static void *xrealloc_typed(void *ptr, size_t size, cmark_malloc_type_id type_id
 
 #define xrealloc_typed_backdeploy xrealloc_typed
 
+END_TYPE_ALLOCATOR_IMPL
+
 #else
 
 static void *xcalloc(size_t nmem, size_t size);
@@ -78,6 +90,10 @@ static void *xrealloc_typed(void *ptr, size_t size, cmark_malloc_type_id type_id
 }
 
 #endif
+
+// When type-aware allocators are available, these functions participate in the
+// type-aware allocations machinery. This behavior is managed in mem.c
+START_TYPE_ALLOCATOR_IMPL
 
 static void *xcalloc(size_t nmem, size_t size) {
   void *ptr = calloc(nmem, size);
@@ -100,6 +116,8 @@ static void *xrealloc(void *ptr, size_t size) {
 static void xfree(void *ptr) {
   free(ptr);
 }
+
+END_TYPE_ALLOCATOR_IMPL
 
 cmark_mem CMARK_DEFAULT_MEM_ALLOCATOR = {xcalloc, xrealloc, xfree, xcalloc_typed, xrealloc_typed};
 
